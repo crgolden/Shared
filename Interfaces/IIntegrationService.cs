6c7cc53c4b1e15a2e1e145b99ac2ce6077741f1d
Chris Golden <crgolden@msn.com>
2019-07-25 04:38:10 -0500

    Adding `IIntegrationService`; removing `SageOptions`

---
+++ Interfaces/IIntegrationService.cs
@@ -0,0 +1,23 @@
+﻿namespace crgolden.Shared
+{
+    using System.Threading;
+    using System.Threading.Tasks;
+
+    public interface IIntegrationService
+    {
+        Task<TModel> Create<TModel, TRequest>(TRequest request, CancellationToken token)
+            where TModel : class;
+
+        Task<TModel> Read<TModel, TRequest>(TRequest request, CancellationToken token)
+            where TModel : class;
+
+        Task Update<TModel, TRequest>(TRequest request, CancellationToken token)
+            where TModel : class;
+
+        Task Delete<TModel, TRequest>(TRequest request, CancellationToken token)
+            where TModel : class;
+
+        Task<TModel[]> List<TModel, TRequest>(TRequest request, CancellationToken token)
+            where TModel : class;
+    }
+}
