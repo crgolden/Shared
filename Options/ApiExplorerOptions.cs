8e48bf17bc02fcb6c9f64a8fd827ea4c2500a07d
Chris Golden <crgolden@msn.com>
2019-09-04 03:06:51 -0500

    Adding odata

---
+++ Options/ApiExplorerOptions.cs
@@ -0,0 +1,31 @@
+﻿namespace crgolden.Shared
+{
+    public class ApiExplorerOptions
+    {
+        public string Title { get; set; }
+
+        public string Description { get; set; }
+
+        public string TermsOfService { get; set; } = "Shareware";
+
+        public string ContactName { get; set; }
+
+        public string ContactEmail { get; set; }
+
+        public string ContactUrl { get; set; }
+
+        public string LicenseName { get; set; } = "MIT";
+
+        public string LicenseUrl { get; set; } = "https://opensource.org/licenses/MIT";
+
+        public string DefaultScheme { get; set; } = "Bearer";
+
+        public string ApiKeySchemeType { get; set; } = "apiKey";
+
+        public string ApiKeySchemeIn { get; set; } = "Header";
+
+        public string ApiKeySchemeName { get; set; } = "Authorization";
+
+        public string ApiKeySchemeDescription { get; set; }
+    }
+}
